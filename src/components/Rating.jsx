310d74e7d768ce55b5ba81e053a9437e2f55569c
aryannema <aaryan.techy@gmail.com>
2026-02-18 18:41:25 +0530

    introduced useState

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
 const Rating = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  const stars = Array.from({ index: 5 }, (_, i) => i + 1);
+
   return (
     <div className="rating-container">
       <h2>Rate your experience</h2>
+      <div className="stars">
+        {stars.map((star) => (
+          <span
+            onMouseEnter={() => setHover(star)}
+            onMouseLeave={() => setHover(0)}
+            onClick={() => setRating(star)}
+            key={star}
+            className="star"
+          >
+            {"\u2605"}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
